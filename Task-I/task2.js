11aa7ac52fa80017e2a900a5766e26fda10bd3e6
Abubakar Shaikh <abubakarshaikhask@gmail.com>
2023-12-24 15:55:44 +0530

    Object Operation logic

---
+++ Task-I/task2.js
@@ -0,0 +1,50 @@
+// Object Operation
+
+/**
+ * @param {*} car1 - The first car object to merge.
+ * @param {*} car2 - The second car object to merge.
+ * @context Merges two car objects into a new object, preserving properties from both.
+ * @returns {Object} mergedCar - The merged car object.
+ */
+function mergeCars(car1, car2) {
+  // Create a new object to store the merged properties
+  let mergedCar = {};
+
+  // Copy properties from the first car object
+  for (let key in car1) {
+    // Check if the property is not inherited from the prototype chain
+    if (car1[key] !== undefined) {
+      mergedCar[key] = car1[key];
+    }
+  }
+
+  // Copy properties from the second car object, overwriting any existing properties
+  for (let key in car2) {
+    // Check if the property is not inherited from the prototype chain
+    if (car2[key] !== undefined) {
+      mergedCar[key] = car2[key];
+    }
+  }
+
+  // Return the merged object
+  return mergedCar;
+}
+
+// Example car objects
+let car1 = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2020,
+};
+
+let car2 = {
+  brand: "Tesla",
+  model: "Model S",
+  year: 2021,
+};
+
+// Merge the two car objects
+let mergedCar = mergeCars(car1, car2);
+
+// Display the merged car object
+console.log(mergedCar);
